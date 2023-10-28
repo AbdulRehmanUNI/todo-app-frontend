648dc251ba2da2b91a5056c09096b40dd9725e43
AbdulRehman56 <211370196@gift.edu.pk>
2023-10-28 23:00:56 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import TaskList from "./components/TaskList"
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
  
- export const URL="https://todo-app-fnlc-api.onrender.com/"; 
+ export const URL="https://todo-app-fnlc-api.onrender.com"; 
 function App() {
   return (
     <div className="app">
